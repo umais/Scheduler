35bbbd451c2fb316f7c25d664cdda6609e9dbc3d
Umais Siddiqui <umais20@yahoo.com>
2019-06-17 16:53:22 -0400

    checked in

--- SchedulerWebApp/Controllers/AccountController.cs
+++ SchedulerWebApp/Controllers/AccountController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
 namespace SchedulerWebApp.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Login()
         {
+            ClaimsPrincipal currentUser =  HttpContext.User;
+            ViewBag.UserName = "None";
+            foreach (var claim in currentUser.Claims)
+            {
+                ViewBag.UserName += claim.Value;
+            }
+
             return View();
         }
     }
